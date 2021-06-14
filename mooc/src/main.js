b68f7dc7a497c0e59fd9f6914bd83aa9b7ba8d2e
ji-jessica <577074958@qq.com>
2021-06-14 10:47:27 +0800

    initial

---
+++ mooc/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import installElementPlus from './plugins/element'
+import router from './router'
+
+
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+
+const app = createApp(App)
+app.use(router)
+app.use(VueAxios,axios);
+installElementPlus(app)
+app.mount('#app')
