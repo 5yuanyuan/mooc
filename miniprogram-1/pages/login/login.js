d308781f17df3510f46ec96950b096be5ac11eed
5yu@nyu@n <52690783+5yuanyuan@users.noreply.github.com>
2021-06-20 22:54:15 +0800

    miniprogram

--- miniprogram-1/pages/login/login.js
+++ miniprogram-1/pages/login/login.js
@@ -16,11 +16,11 @@ Page({
     var code;
     // 首先默认code为空字符串
     code = '';
-    // 设置长度，这里看需求，我这里设置了4
+    // 设置长度
     var codeLength = 4;
     //设置随机字符
     var random = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
-    // 循环codeLength 我设置的4就是循环4次
+    // 循环codeLength
     for (var i = 0; i < codeLength; i++) {
       // 设置随机数范围,这设置为0 ~ 36
       var index = Math.floor(Math.random() * 36);
--- miniprogram-1/pages/usercenter/usercenter.js
+++ miniprogram-1/pages/usercenter/usercenter.js
@@ -1,6 +1,12 @@
 var app = getApp();
 Page({
 
+  onShareAppMessage() {
+    return {
+      title: 'tabs'
+    }
+  },
+
   data: {
     loginUrl: `${app.globalData.baseurl}/api/member/login`,
     userInfoUrl: `${app.globalData.baseurl}/api/member/auth/getMemberInfo`,
@@ -19,11 +25,28 @@ Page({
       bindtap: 'viewImage',
       txt: '查看头像'
     }
-    ] // 底部可选菜单
+    ], // 底部可选菜单
+    tabs: [],
+    activeTab: 0,
   },
 
   onLoad: function (options) {
     this.getUserInfo()
+    const tabs = [
+      {
+        title: '登录',
+        title2: '小程序开发进阶',
+        img: 'http://mmbiz.qpic.cn/sz_mmbiz_jpg/GEWVeJPFkSEV5QjxLDJaL6ibHLSZ02TIcve0ocPXrdTVqGGbqAmh5Mw9V7504dlEiatSvnyibibHCrVQO2GEYsJicPA/0?wx_fmt=jpeg',
+        desc: '本视频系列课程，由腾讯课堂NEXT学院与微信团队联合出品，通过实战案例，深入浅出地进行讲解。',
+      },
+      {
+        title: '注册',
+        title2: '微信小程序直播',
+        img: 'http://mmbiz.qpic.cn/sz_mmbiz_png/GEWVeJPFkSHALb0g5rCc4Jf5IqDfdwhWJ43I1IvriaV5uFr9fLAuv3uxHR7DQstbIxhNXFoQEcxGzWwzQUDBd6Q/0?wx_fmt=png',
+        desc: '微信小程序直播系列课程持续更新中，帮助大家更好地理解、应用微信小程序直播功能。',
+      }
+    ]
+    this.setData({ tabs })
   },
 
   // 获取用户信息
@@ -132,4 +155,24 @@ Page({
       url: '../regist/regist',
     })
   },
+
+  onTabClick(e) {
+    const index = e.detail.index
+    this.setData({ 
+      activeTab: index 
+    })
+  },
+
+  onChange(e) {
+    const index = e.detail.index
+    this.setData({ 
+      activeTab: index 
+    })
+  },
+
+  handleClick(e) {
+    wx.navigateTo({
+      url: './webview',
+    })
+  }
 })
